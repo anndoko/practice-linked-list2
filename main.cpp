199a5bbd5809de9fded584ab67593f4a41ad8a99
Anndo <handcuffanndo@gmail.com>
2016-05-06 22:07:10 +0800

    Rewrite: remove global variable and rewrite Insert and Print

--- main.cpp
+++ main.cpp
@@ -12,40 +12,37 @@ struct Node{
     //pointer to node struct
 };
 
-struct Node* head;
-
 //----------insert a number to the list----------//
-void Insert(int x){
+Node* Insert(Node* head, int x){
     Node* temp = (Node*)malloc(sizeof(struct Node));
     temp->data = x;
     temp->link = head;
     head = temp;
+    return head;
 }
 
 
 //----------display the entire list----------//
-void Print(){
-    struct Node* temp = head; //collect the address stored in head
+void Print(Node* head){
     cout << "List: ";
-    while(temp != NULL){
-        cout << temp->data << " ";
-        temp = temp->link;
+    while(head != NULL){
+        cout << head->data << " ";
+        head = head->link;
     }
     cout << endl;
 }
 
 //----------MAIN----------//
 int main() {
-    
-    head = NULL; //empty list
+    Node* head = NULL; //empty list
     cout << "How many numbers?" << endl;
     int n, i, x;
     cin >> n;
     for(i = 0; i < n; i++){
         cout << "Enter the number: " << endl;
         cin >> x;
-        Insert(x);
-        Print();
+        head = Insert(head, x);
+        Print(head);
     }
     return 0;
 }
